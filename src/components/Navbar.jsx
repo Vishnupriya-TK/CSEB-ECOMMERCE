20f7569810683bad855b6d13a795a055c8a5b3c0
Vishnu Priya T K <tkvishnupriyacse@gmail.com>
2025-08-01 22:28:55 +0530

    Navbar  updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
   const menuItems = [
     { name: 'Home', path: '/' },
     { name: 'Products', path: '/products' },
+    { name: 'Orders', path: '/orders' },
     { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
     { name: 'Cart', path: '/cart' },
